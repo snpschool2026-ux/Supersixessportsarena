b272a2eb28b8d8338c9413bc8eb8bf270dd80027
SuperSixesSportsArena <snpschool2026@gmail.com>
2026-09-10 12:19:22 +0530

    Update sw.js

--- admin/sw.js
+++ admin/sw.js
@@ -1,8 +1,2 @@
-const CACHE = 'warehouse-pro-admin-v1';
-const ASSETS = ['./', './index.html', './manifest.webmanifest'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
